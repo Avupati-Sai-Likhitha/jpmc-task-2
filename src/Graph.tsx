3d4e3a2533271213d8472fa85be03af3f086f46b
Avupati-Sai-Likhitha <81608276+Avupati-Sai-Likhitha@users.noreply.github.com>
2022-11-17 23:24:58 +0530

    Update Graph.tsx

--- src/Graph.tsx
+++ src/Graph.tsx
@@ -6,6 +6,16 @@ import './Graph.css';
 /**
  * Props declaration for <Graph />
  */
+elem.setAttribute('view', 'y_line');
+elem.setAttribute('column-pivots', '["stock"]');
+elem.setAttribute('row-pivots', '["timestamp"]');
+elem.setAttribute('columns', 'top_ask_price');
+elem.setAttribute('aggregates',
+                  {"stock":"distinct count",
+                   "top_ask_price":"avg",
+                   "top_bid_price":"avg",
+                   "timestamp":"distinct count"}');
+
 interface IProps {
   data: ServerRespond[],
 }
@@ -14,7 +24,7 @@ interface IProps {
  * Perspective library adds load to HTMLElement prototype.
  * This interface acts as a wrapper for Typescript compiler.
  */
-interface PerspectiveViewerElement {
+interface PerspectiveViewerElement extends HTMLElement{
   load: (table: Table) => void,
 }
 
@@ -32,7 +42,7 @@ class Graph extends Component<IProps, {}> {
 
   componentDidMount() {
     // Get element to attach the table from the DOM.
-    const elem: PerspectiveViewerElement = document.getElementsByTagName('perspective-viewer')[0] as unknown as PerspectiveViewerElement;
+    const elem = document.getElementsByTagName('perspective-viewer')[0] as unknown as PerspectiveViewerElement;
 
     const schema = {
       stock: 'string',
